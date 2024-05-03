a5a17cd211d7a9b55494dc7bb326e447b0752fc7
dicaf <diego.caro.7e5@itb.cat>
2024-05-03 19:56:18 +0200

    retoques finales 1.0

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -9,21 +9,27 @@ import kotlinx.serialization.json.Json
 import models.Mensaje
 
 suspend fun main() {
-    val client = HttpClient(CIO)
-    val mensajeEnviado = "ping"
-    val response: HttpResponse = client.post("http://localhost:8080/ping") {
-        contentType(ContentType.Application.Json)
-        setBody(Json.encodeToString(Mensaje(mensajeEnviado)))
-    }
-    println("Status del POST: ${response.status}")
+    var contador = 0
+    while (true) {
+        val client = HttpClient(CIO)
+        val mensajeEnviado = "ping"
+        val response: HttpResponse = client.post("http://localhost:8080/ping") {
+            contentType(ContentType.Application.Json)
+            setBody(Json.encodeToString(Mensaje(mensajeEnviado)))
 
-    val responseText = response.body<String>()
-    println("Respuesta del servidor: $responseText")
+        }
+        if (contador == 0 || contador % 10 == 0) {
+            println("Status del POST: ${response.status}")
+        }
+
+
+        val responseText = response.body<String>()
 
         val mensaje = Json.decodeFromString<Mensaje>(responseText)
-        println("Mensaje que le envio al servidor $mensajeEnviado")
-        println("Mensaje del servidor: ${mensaje.mensaje}")
-
-
-    client.close()
+        println("Mensaje que le envio al servidor: $mensajeEnviado")
+        println("Mensaje del servidor: ${mensaje.mensaje}\n")
+        contador++
+        Thread.sleep(2000)
+    }
+    //client.close()
 }
